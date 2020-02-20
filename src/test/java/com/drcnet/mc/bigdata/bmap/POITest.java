8bf44264472f9a3d43017957909aee47cefdabf7
tengchao.li <976225259@qq.com>
2020-02-20 21:50:45 +0800

    first

--- src/test/java/com/drcnet/mc/bigdata/bmap/POITest.java
+++ src/test/java/com/drcnet/mc/bigdata/bmap/POITest.java
@@ -16,10 +16,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 /**
